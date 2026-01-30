2dd7cef220edf6ba47cab257fa492676379928d6
Gabriel Oyola <oyolasjose@gmail.com>
2026-01-30 18:47:06 -0500

    refactor(fortify): remove unused function acording bussiness logic

--- config/fortify.php
+++ config/fortify.php
@@ -145,13 +145,13 @@ return [
 
     'features' => [
         Features::registration(),
-        Features::resetPasswords(),
+        /* Features::resetPasswords(),
         Features::emailVerification(),
         Features::twoFactorAuthentication([
             'confirm' => true,
             'confirmPassword' => true,
             // 'window' => 0
-        ]),
+        ]), */
     ],
 
 ];
